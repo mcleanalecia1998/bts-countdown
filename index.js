2c1a71a5d6bae16f00d8f1503f9f8569f8dc6a09
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2024-01-02 20:57:51 -0500

    Added html pages and CSS

--- index.js
+++ index.js
@@ -1,6 +1,99 @@
-timeElement = document.querySelector("timer");
+// set countdown date
 
-// calculate difference between time
-eventTime = moment.tz("2024-6-12", "Asia/Seoul");
-currentTime = moment.tz();
-duration = moment.duration(eventTime.diff(currentTime));
+function jinComesHome() {
+  let jinCountDownDate = new Date("June 12, 2024").getTime();
+  let now = new Date().getTime();
+  let distance = jinCountDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("jin-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("jin-timer").innerHTML = `Jin is back`;
+  }
+}
+
+function hobiComesHome() {
+  let hobiCountDownDate = new Date("Oct 17, 2024").getTime();
+  let now = new Date().getTime();
+  let distance = hobiCountDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("hobi-timer").innerHTML =
+    days + "D " + hours + "H " + minutes + "M " + seconds + "S ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hobi-timer").innerHTML = `Hobi is Home!!`;
+  }
+}
+
+function namTaeComesHome() {
+  let namTaeCountDownDate = new Date("June 10, 2025").getTime();
+  let now = new Date().getTime();
+  let distance = namTaeCountDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("namtae-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById(
+      "namtae-timer"
+    ).innerHTML = `Namjoon and Tae are back`;
+  }
+}
+
+function jikookComesHome() {
+  let jikookCountDownDate = new Date("June 11, 2025").getTime();
+  let now = new Date().getTime();
+  let distance = jikookCountDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("jikook-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("jikook-timer").innerHTML = `Hobi is back`;
+  }
+}
+
+function sugaComesHome() {
+  let sugaCountDownDate = new Date("June 21, 2025").getTime();
+  let now = new Date().getTime();
+  let distance = sugaCountDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("suga-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("suga-timer").innerHTML = `Hobi is back`;
+  }
+}
+
+setInterval(jinComesHome, 1000);
+setInterval(hobiComesHome, 1000);
+setInterval(namTaeComesHome, 1000);
+setInterval(jikookComesHome, 1000);
+setInterval(sugaComesHome, 1000);
